326267a0ea2a1291ac249d1708f445482893de04
evelasco2 <evelasco2@hawk.iit.edu>
2022-11-05 15:26:09 -0500

    added semicolon

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ function init() {
 //add your javascript between these two lines of code
   function myEventFunction() {
     var inp = document.getElementById('entryinput');
-    alert('Eduardo Velasco ' + inp.value);
+    alert('Eduardo Velasco: ' + inp.value);
 
     var textout = document.getElementById('textoutput')
     textout.innherHTML = inp.value;
   }
 
   var entrybut =document.getElementById('entrybutton');
-  entrybut.addEventListener('click' myEventFunction);
+  entrybut.addEventListener('click' myEventFunction());
 }
 
 window.addEventListener('load', init);
