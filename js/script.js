93cb3024026ea8f048641ecb4dedbbd98c0d0856
evelasco2 <evelasco2@hawk.iit.edu>
2022-11-05 14:40:32 -0500

    added event listeners and javascript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 function init() {
 //add your javascript between these two lines of code
- 
-status
-alert()
-confirm()
+  function myEventFunction() {
+    var inp = document.getElementById('entryinput');
+    alert(inp.value);
+  }
 
+  entrybutton.addEventListener('click' myEventFunction);
 
-
-
-
+  entrybutton.addEventListener('click' function) {
+    textoutput.innherHTML = 'inp.value';
+  }
+}
 
 window.addEventListener('load', init);
